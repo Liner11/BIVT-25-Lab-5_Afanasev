daf4413391219791865c0557634671d1dd40ee87
Liner <133678221+Liner11@users.noreply.github.com>
2025-11-25 20:16:01 +0300

    Notebook

--- Lab5/Purple.cs
+++ Lab5/Purple.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 
 namespace Lab5
@@ -10,7 +11,15 @@ namespace Lab5
             int[] answer = null;
 
             // code here
-
+            answer = new int[matrix.GetLength(1)];
+            int currentcount;
+            for(int y = 0; y < matrix.GetLength(1); y++)
+            {
+                currentcount = 0;
+                for (int x = 0; x < matrix.GetLength(0); x++) if (matrix[x, y] < 0) currentcount++;
+                answer[y] = currentcount;
+            }
+            //YES
             // end
 
             return answer;
@@ -19,7 +28,41 @@ namespace Lab5
         {
 
             // code here
-
+            int minEl;
+            int temp;
+            int temp1;
+            int[,] answer = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                minEl = matrix[x, 0];
+                for (int y = 0; y < matrix.GetLength(1); y++) 
+                {
+                    if (matrix[x, y] < minEl) 
+                    {
+                        minEl = matrix[x, y];
+                    }
+                }
+                if(minEl != matrix[x, 0])
+                {
+                    temp = matrix[x, 0];
+                    matrix[x, 0] = minEl;
+                    for (int y = 1; y < matrix.GetLength(1); y++)
+                    {
+                        if (matrix[x, y] != minEl)
+                        {
+                            temp1 = matrix[x, y];
+                            matrix[x, y] = temp;
+                            temp = temp1;
+                        }
+                        else if (matrix[x, y] == minEl)
+                        {
+                            matrix[x, y] = temp;
+                            break;
+                        }
+                    }
+                }
+            }
+            //YES
             // end
 
         }
@@ -28,7 +71,33 @@ namespace Lab5
             int[,] answer = null;
 
             // code here
-
+            int maxEl;
+            bool isA;
+            answer = new int[matrix.GetLength(0), matrix.GetLength(1) + 1];
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                maxEl = matrix[x, 0];
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    if (matrix[x, y] > maxEl)
+                    {
+                        maxEl = matrix[x, y];
+                    }
+                }
+                isA = false;
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    if (matrix[x, y] == maxEl && !isA)
+                    {
+                        isA = true;
+                        answer[x, y] = matrix[x, y];
+                        answer[x, y + 1] = maxEl;
+                    }
+                    else if (!isA) answer[x, y] = matrix[x, y];
+                    else answer[x, y + 1] = matrix[x, y];
+                }
+            }
+            //YES
             // end
 
             return answer;
@@ -37,23 +106,73 @@ namespace Lab5
         {
 
             // code here
+            int maxEl;
+            int sum;
+            int count;
+            bool isA;
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                maxEl = matrix[x, 0];
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    if (matrix[x, y] > maxEl)
+                    {
+                        maxEl = matrix[x, y];
+                    }
+                }
+                isA = false;
+                count = 0;
+                sum = 0;
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    if (matrix[x, y] == maxEl)
+                    {
+                        isA = true;
+                    }
+                    else if (matrix[x, y] > 0 && isA)
+                    {
+                        count++;
+                        sum += matrix[x, y];
+                    }
+                }
 
+            }
+            //CHECK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             // end
 
-        }
+            }
         public void Task5(int[,] matrix, int k)
         {
 
             // code here
-
+            int[] maxLine = new int[matrix.GetLength(0)];
+            int maxEl;
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                maxEl = matrix[x, 0];
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    if (matrix[x, y] > maxEl)
+                    {
+                        maxEl = matrix[x, y];
+                    }
+                }
+                maxLine[x] = maxEl;
+            }
+            for(int y = matrix.GetLength(0) - 1; y >= 0; y--)
+            {
+                matrix[y, k] = maxLine[matrix.GetLength(0) - y - 1];
+            }
+            
+            //YES
             // end
 
-        }
+            }
         public void Task6(int[,] matrix, int[] array)
         {
 
             // code here
-
+            
             // end
 
         }
@@ -61,7 +180,58 @@ namespace Lab5
         {
 
             // code here
-
+            int[] indexDependence = new int[matrix.GetLength(0)];
+            int[] minDependence = new int[matrix.GetLength(0)];
+            int[,] answer = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            int minEl;
+            int temp;
+            int indexTemp;
+            for(int y = 0; y < matrix.GetLength(0); y++)
+            {
+                minEl = matrix[y, 0];
+                for(int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    if (matrix[y, x] < minEl)
+                    {
+                        minEl = matrix[y, x];
+                    }
+                }
+                indexDependence[y] = y;
+                minDependence[y] = minEl;
+            }
+            temp = minDependence[0];
+            indexTemp = 0;
+            for (int i = 0; i < minDependence.Length; i++)
+            {
+                for(int j = 0; j < minDependence.Length - i - 1; j++)
+                {
+                    if (minDependence[j + 1] > minDependence[j])
+                    {
+                        temp = minDependence[j];
+                        minDependence[j] = minDependence[j + 1];
+                        minDependence[j + 1] = temp;
+                        indexTemp = indexDependence[j];
+                        indexDependence[j] = indexDependence[j + 1];
+                        indexDependence[j + 1] = indexTemp;
+                    }
+                }
+            }
+            for(int y = 0; y < answer.GetLength(0); y++)
+            {
+                for(int x = 0; x < answer.GetLength(1); x++)
+                {
+                    answer[y, x] = matrix[indexDependence[y], x];
+                }
+            }
+            for (int y = 0; y < matrix.GetLength(0); y++)
+            {
+                for (int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    matrix[y, x] = answer[y, x];
+                }
+            }
+            
+            //YES
             // end
 
         }
@@ -70,7 +240,14 @@ namespace Lab5
             int[] answer = null;
 
             // code here
+            int n = matrix.GetLength(0);
+            if(n == matrix.GetLength(1))
+            {
+                answer = new int[(n * 2) - 1];
+            }
 
+            MatrixShow MS = new MatrixShow();
+            MS.ShowMatrix(matrix);
             // end
 
             return answer;
